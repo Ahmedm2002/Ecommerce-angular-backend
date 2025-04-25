4598ec98d506581f5b5c30d581f1358c7a1307f5
ahmed-tecklogics <ahmed.tecklogics@gmail.com>
2025-04-25 23:55:30 +0500

    Added morgan and connect mongoDb

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
-require("dotenv").config();
+const dotenv = require("dotenv")
 const express = require("express");
 const app = express();
 const port = 3000;
-const userRoutes = require("./routes/products/productRoutes");
-const productRoutes = require("./routes/users/userRoutes");
+const userRoutes = require("./src/routes/products/productRoutes");
+const productRoutes = require("./src/routes/users/userRoutes");
 const mongoose = require("mongoose");
 
+dotenv.config()
 console.log(process.env.MONGO_URL);
 
-mongoose
+if(process.env.MONGO_URL){
+  mongoose
   .connect(process.env.MONGO_URL)
   .then(() => {
     console.log("Databse Connected");
@@ -16,8 +18,10 @@ mongoose
   .catch((error) => {
     console.log("Databse connection failed", error);
   });
+}else{
+  console.log(`Mongo_url not found`)
+}
 
-const con = mongoose.connection;
 
 app.use("/user", userRoutes);
 app.use("/product", productRoutes);
